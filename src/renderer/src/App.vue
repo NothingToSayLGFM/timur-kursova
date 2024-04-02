62fa835e56274c8ce0a3e41c3d54143eb60db6b1
Ihor Pysarenko <Ihor.Pysarenko@securecrowds.com.ua>
2024-04-03 01:24:44 +0300

    a

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>some</p>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import axios from 'axios'
+
+onMounted(async () => {
+  const a = await axios.get('http://localhost:3000/tasks')
+  console.log(a)
+})
+</script>
